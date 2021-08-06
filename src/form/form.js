4e4eb872218814db4683377bb6f3f2c782076d6d
demba KANTE <b-demba@hotmail.fr>
2021-08-05 19:45:20 -0700

    blog

--- src/form/form.js
+++ src/form/form.js
@@ -1,28 +1,37 @@
+//on importe les bibliothéques
+
 import "./form.css";
 import * as axios from "axios";
+//on récupére un reférence au DOM HTML
 
 const form = document.querySelector("form");
 
 const back = document.querySelector(".back");
 
+//on crée une constante qui va récuperer l'url
 let url;
-
+//on crée un event listener qui va permettre de retourner a la page précedentes
 back.addEventListener("click", (e) => {
   history.back(1);
 });
 
+//on crée un event listener qui va permettre de valider le form 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  //on recupere les données des inputs du form
   const PostData = new FormData(form);
 
+  //on transforme ces inputs en un objet
   const Post = Object.fromEntries(PostData);
-
+//on convertir cet objet aux fromat json
   const post = JSON.stringify(Post);
 
+  //on crée un let pour pourvoir effectuer des promess en fonction de l'action ajouter ou modifier
   let Promess;
 
   if (url) {
+    //on mofifie l'article
     Promess = fetch("https://restapi.fr/api/Post", {
       method: "PATCH",
 
@@ -32,6 +41,7 @@ form.addEventListener("submit", (e) => {
       },
     });
   } else {
+    //on ajoute l'article
     Promess = fetch("https://restapi.fr/api/Post", {
       method: "POST",
 
@@ -43,15 +53,18 @@ form.addEventListener("submit", (e) => {
   }
 
   Promess.then((res) => console.log(res)).catch((err) => console.log(err));
+//on retourn a index.html
   location.assign("index.html");
 });
 
+//on crée une fonction qui va récupere l'url et afficher le form avec les infos du post sélectionner
 const EditForm = () => {
   url = new URL(location.href).searchParams.get("id");
 
   return DisplayEdit(url);
 };
 
+//on crée une fonction qui va recupere le post en question
 const DisplayEdit = async (url) => {
   const Post = await fetch(`https://restapi.fr/api/Post/${url}`);
 
@@ -59,13 +72,14 @@ const DisplayEdit = async (url) => {
     const response = await Post.json();
 
     console.log(response);
-
+//on return la foction fiilform
     return fillform(response);
   } catch (e) {
     console.log(e);
   }
 };
 
+// cette foction va récupere les inputs du post sélectionner
 const fillform = (response) => {
   const nom = document.querySelector('input[name="nom"]');
   const prenom = document.querySelector('input[name="prenom"]');
@@ -82,4 +96,5 @@ const fillform = (response) => {
   publication.value = response.publication || "";
 };
 
+//on instancie la fonction EditForm
 EditForm();
